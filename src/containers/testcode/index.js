0334ce6b88aec80cb848480cc22cf3df124e611d
dondon02 <dondon@onwardsmg.com>
2018-03-05 10:21:00 +0800

    remove console messages

--- src/containers/testcode/index.js
+++ src/containers/testcode/index.js
@@ -9,41 +9,38 @@ import { Card, Collapse } from 'antd';
 const Panel = Collapse.Panel;
 
 class TestCode extends Component {
-	constructor(props) {
-		super(props);
-		console.log('asdf');
-	    this.state = {
-	      key: "1",
-	    }
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      key: '1'
+    };
+  }
 
-	callback = key => {
-		console.log(key);
-		this.setState({ key: key.toString() });
-	}
+  callback = key => {
+    this.setState({ key: key.toString() });
+  };
 
-	render() {
-		const { key } = this.state;
-		return (
-			<Card title="Test Code">
-				<Collapse activeKey={key} accordion onChange={this.callback}>
-					<Panel header="Fibonacci" key="1">
-						<Fibonacci />
-					</Panel>
-					<Panel header="Sum of all unique numbers" key="2">
-						<Sum />
-					</Panel>
-					<Panel header="Bigger Value of Permutation" key="3">
-						<Permutations />
-					</Panel>
-					<Panel header="Combine 2 Array" key="4">
-						<CombineArray />
-					</Panel>
-				</Collapse>
-			</Card>
-		);
-	}
-};
+  render() {
+    const { key } = this.state;
+    return (
+      <Card title="Test Code">
+        <Collapse activeKey={key} accordion onChange={this.callback}>
+          <Panel header="Fibonacci" key="1">
+            <Fibonacci />
+          </Panel>
+          <Panel header="Sum of all unique numbers" key="2">
+            <Sum />
+          </Panel>
+          <Panel header="Bigger Value of Permutation" key="3">
+            <Permutations />
+          </Panel>
+          <Panel header="Combine 2 Array" key="4">
+            <CombineArray />
+          </Panel>
+        </Collapse>
+      </Card>
+    );
+  }
+}
 
 export default TestCode;
-
